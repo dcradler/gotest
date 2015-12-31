b3f98e45de49a6631d087c87446f3a15780bc383
Cradler <dcradler@trueblue.com>
2015-12-31 15:28:57 -0800

    some stuff

--- gotest.go
+++ gotest.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Shownum() {
+	for i :=0; i<100; i++ {
+		foo := rand.Intn(100)
+		fmt.Println(foo)
+		if foo < 10 {
+			break
+		}
+	}
+}
 
 func main() {
-	fmt.Printf("dude!\n")
+	now := time.Now()
+	secs := now.Unix()
+	rand.Seed(secs)
+	fmt.Println("dude!")
+	Shownum()
 }
 
